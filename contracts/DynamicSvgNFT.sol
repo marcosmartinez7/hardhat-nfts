422a1d648fdb9e9fc7e236be98e3967b52073d69
Marcos Martínez <mmartinezciompi@gmail.com>
2022-08-01 17:26:34 -0300

    .env example

--- contracts/DynamicSvgNFT.sol
+++ contracts/DynamicSvgNFT.sol
@@ -1,16 +1,23 @@
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
 pragma solidity ^0.8.15;
 
 contract DynamicSvgNft is ERC721 {
     uint256 s_tokenCounter;
     string private i_lowImageUri;
     string private i_highImageUri;
+    string private constant base64EncodedSvgPrexix = "data:image/svg+xml;base64,"
 
     constructor(string memory lowSvg, string memory highSvg)
         ERC721("SVG NFT", "SVGNFT")
     {}
+
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(base64EncodedSvgPrexix, svgBase64Encoded));
+    }
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
